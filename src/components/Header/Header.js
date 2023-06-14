7d7f6026bb07f089f5a340c5d64f0cea3c0c8207
zeinab <zeinab@codi.tech>
2023-06-14 15:07:28 +0300

    finishing the header menu in mobile version

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ClickAwayListener } from '@mui/base';
 import "./Header.css";
 import { BiUser, BiCart, BiMenu } from 'react-icons/bi';
 import { Link } from "react-router-dom";
 
 
 function Header(){
+
+    const[ openMobileMenu , setOpenMobileMenu] = useState(0)
+
+    const toggleMobileMenu = ()=>{
+        if(openMobileMenu === 1){
+            document.body.style.overflow = "scroll";
+            setOpenMobileMenu(0);
+        }
+        else{
+            setOpenMobileMenu(1);
+            document.body.style.overflow = "hidden"
+        }
+    }
+
+    const handleClickAway = ()=>{
+        document.body.style.overflow = "scroll";
+        document.getElementById("checkbox").click()
+        setOpenMobileMenu(0);
+    }
 
     return(
             <div className={window.location.pathname === '/' ? 'header header_home' : 'header'}>
@@ -24,18 +44,32 @@ function Header(){
                         <div className="header_menu">
                             <BiUser size={30}/>
                         </div>
-                        <div>
-
-                            <input type="checkbox" id="checkbox" />
-                            <label for="checkbox" class="toggle">
-                                <div class="bars" id="bar1"></div>
-                                <div class="bars" id="bar2"></div>
-                                <div class="bars" id="bar3"></div>
-                            </label>
-                        </div>
-
                     </div>
                 </div>
+                <ClickAwayListener onClickAway={handleClickAway}>
+
+                <div className="mobile_menu">
+                    <div> logo</div>
+                    <div>
+                        <input type="checkbox" id="checkbox" />
+                        <label for="checkbox" class="toggle" onClick={toggleMobileMenu}>
+                            <div class="bars" id="bar1"></div>
+                            <div class="bars" id="bar2"></div>
+                            <div class="bars" id="bar3"></div>
+                        </label>
+                        
+                        <div className={openMobileMenu === 0 ? "mobile_menu_page_hidden" : "mobile_menu_page" }>
+                            <Link className="mobile_menu_list" to="/" onClick={handleClickAway}>Home</Link>
+                            <Link className="mobile_menu_list" to="/product" onClick={handleClickAway}>PRODUCTS</Link>
+                            <Link className="mobile_menu_list" to="/about" onClick={handleClickAway}>ABOUT</Link>
+                            <Link className="mobile_menu_list" to="/contact" onClick={handleClickAway}>CONTACT US</Link>
+                        </div>
+                        
+                    </div>
+               
+                </div>
+                </ClickAwayListener>
+
             </div>
     )
 }
